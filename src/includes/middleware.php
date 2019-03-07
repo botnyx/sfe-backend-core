0a2684a7a29b5b85737019e25b47846940879e28
botnyx <chlsbkrs@gmail.com>
2019-03-07 11:58:37 +0100

    ..

--- src/includes/middleware.php
+++ src/includes/middleware.php
@@ -1,1 +1,40 @@
 <?php
+
+
+<?php
+/**
+*	Middleware for the backend role.
+*
+*
+*
+*
+**/
+
+if(!array_key_exists('pdo',$container)){
+	$container['pdo'] = $pdo;
+}
+
+// Register globally to app
+$container['session'] = function ($c) {
+  return new \SlimSession\Helper;
+};
+
+
+
+$backendMiddleware = function ($request, $response, $next)  {
+
+	$clientIssuer=_SETTINGS['sfeBackend']['sfeAuthSrv'];//"https://auth.devpoc.nl";
+
+	$sfeBackendMiddleWare = new \Botnyx\SfeBackend\sfeBackendMiddleWare($request, $response,$this->pdo,$clientIssuer );
+	// Add requestAttributes.
+	$request = $sfeBackendMiddleWare->addRequestAttributes($request);
+	$response = $next($request, $response);
+	// Add responseHeaders.
+	$response = $sfeBackendMiddleWare->addResponseHeaders($response);
+	return $response;;//->withHeader('Access-Control-Allow-Origin','*');
+};
+/*
+*/
+
+/**/
+$app->add($backendMiddleware);
