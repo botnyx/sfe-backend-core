8b6ada38fbbcadd18bf4ebf1ac26102132de090a
botnyx <chlsbkrs@gmail.com>
2019-03-07 01:27:34 +0100

    Update routes.php

--- src/includes/routes.php
+++ src/includes/routes.php
@@ -1,1 +1,9 @@
-<?php 
+<?php
+
+
+
+	$app->get('/robots.txt',  function ( $request,  $response, array $args){
+		$res = "User-agent: *".PHP_EOL."Disallow: /";
+		return $response->write($res);
+
+	});
