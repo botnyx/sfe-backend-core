9b608a908f4e665e9003efa4b529a99c028e4d24
botnyx <chlsbkrs@gmail.com>
2019-03-10 20:34:14 +0100

    Update Backend.php

--- src/Core/Setup/Backend.php
+++ src/Core/Setup/Backend.php
@@ -9,65 +9,37 @@ use Composer\Installer\PackageEvent;
 
 class Backend {
     
+	function construct( $vendorDir){
+		$this->vendorDir = $vendorDir;
+		$this->projectDir = realpath($vendorDir . '/..');
+		
+		$this->installedComponents = json_decode(\Botnyx\Sfe\Backend\Core\Setup\Backend::readfile($vendorDir."/composer/installed.json"));
+		
+	}
+	
+	
 	static function postUpdate(Event $event){
+		
 		//$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
 		$vendorDir = realpath($event->getComposer()->getConfig()->get('vendor-dir'));
-     	$projectDir = realpath($vendorDir . '/..');
-		$vendorDir."/composer/installed.json";
 		
 		
-		$installed = json_decode(\Botnyx\Sfe\Backend\Core\Setup\Backend::readfile($vendorDir."/composer/installed.json"));
+		$installer = new \Botnyx\Sfe\Backend\Core\Setup\Backend($vendorDir);
+		$installer->update();
 		
-		echo "\n";
-		echo "Updating: `".$installed[0]->name."`\n";
-		echo "version: `".$installed[0]->version."`\n";
-		//$installed[0]->version_normalized;
-		
-		
-		#var_dump("xyz");
-		echo "-----------------------------------------\n";
-		
-		$searchDir = realpath($vendorDir . '/../..');
-		if( file_exists($searchDir."/configuration.ini") ){
-			echo "found a previous `configuration.ini` ( ".$searchDir."/configuration.ini"." ) \n";	
-			echo "need to add config-parse code here..";
-		}
-		
-		//echo "Database setup/update should go here\n";
-		
-		//print_r($projectDir );
-		echo "\n";
 	}
 	
 	static function postInstall(Event $event){
 		
 		//$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
 		$vendorDir = realpath($event->getComposer()->getConfig()->get('vendor-dir'));
-     	$projectDir = realpath($vendorDir . '/..');
 		
 		
-		$vendorDir."/composer/installed.json";
+		$installer = new \Botnyx\Sfe\Backend\Core\Setup\Backend($vendorDir);
+		$installer->setup();
 		
 		
-		$installed = json_decode(\Botnyx\Sfe\Backend\Core\Setup\Backend::readfile($vendorDir."/composer/installed.json"));
-		
-		echo "\n";
-		echo "Setting up: `".$installed[0]->name."`\n";
-		echo "version: `".$installed[0]->version."`\n";
-		//$installed[0]->version_normalized;
-		
-		
-		#var_dump("xyz");
-		echo "-----------------------------------------\n";
-		
-		$searchDir = realpath($vendorDir . '/../..');
-		if( file_exists($searchDir."/configuration.ini") ){
-			echo "found a previous `configuration.ini` ( ".$searchDir."/configuration.ini"." ) \n";	
-			echo "need to add config-parse code here..";
-		}
-		echo "\n";
 	}
-	
 	
 	static function readfile ($filename){
 		$handle = fopen($filename, "rb");
@@ -75,5 +47,87 @@ class Backend {
 		fclose($handle);
 		return $contents;
 	}
+	
+	public function update (){
+		echo "\n";
+		echo "Updating: `".$this->installedComponents[0]->name."`\n";
+		echo "version: `".$this->installedComponents[0]->version."`\n";
+		//$installed[0]->version_normalized;
+		
+		#var_dump("xyz");
+		echo "-----------------------------------------\n";
+		echo "\n\nUNFINISHED!!\n\n";
+		
+	}
+	public function setup (){
+		echo "\n";
+		echo "Setting up: `".$this->installedComponents[0]->name."`\n";
+		echo "version: `".$this->installedComponents[0]->version."`\n";
+		//$installed[0]->version_normalized;
+		
+		#var_dump("xyz");
+		echo "-----------------------------------------\n";
+		
+		$searchDir = realpath($this->vendorDir . '/../..');
+		if( file_exists($searchDir."/configuration.ini") ){
+			echo "found a previous `configuration.ini` ( ".$searchDir."/configuration.ini"." ) \n";	
+			echo "need to add config-parse code here..\n";
+			
+			echo "\n\nUNFINISHED!!\n\n";
+		}else{
+			echo "\n No configuration found, starting setup.\n";
+			$public_html = $this->public_html();
+			$tempFolder = $this->tempFolder();
+			$dbCredentials = $this->dbCredentials();
+			echo "\n\nUNFINISHED!!\n\n";
+		}
+		echo "\n";
+	}
+	
+	
+	
+	
+	private function dbCredentials(){
+		echo "---------------------\n";
+		echo "Configuration step 3.\n";
+		echo "Please enter the connection dsn  (mysql:host=localhost;dbname=backend)\n";
+		$dsn = rtrim(fgets(STDIN));
+		
+		echo "Please enter the database username \n";
+		$user = rtrim(fgets(STDIN));
+		echo "Please enter the database password \n";
+		$pass = rtrim(fgets(STDIN));
+		
+		
+		return array("dsn"=>$dsn,"user"=>$user,"pass"=>$pass);
+	}
+	
+	
+	private function public_html(){
+		echo "---------------------\n";
+		echo "Configuration step 1.\n";
+		echo "Please enter the location of your web-facing directory \n";
+		$input = rtrim(fgets(STDIN));
+		
+		if( !file_exists( $input )){
+			echo "Error: this location doesnt exist. try again.\n\n\n";
+			$this->public_html();
+		}
+		return $input;
+	}
+	
+	private function tempFolder(){
+		echo "---------------------\n";
+		echo "Configuration step 2.\n";
+		echo "Please enter the location of your /tmp directory \n";
+		$input = rtrim(fgets(STDIN));
+		
+		if( !file_exists( $input )){
+			echo "Error: this location doesnt exist. try again.\n\n\n";
+			$this->tempFolder();
+		}
+		return $input;
+	}
+	
 	
 }
