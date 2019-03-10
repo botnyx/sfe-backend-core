2dea1e6eaa05574873762022052fb06923d32e24
botnyx <chlsbkrs@gmail.com>
2019-03-10 19:28:59 +0100

    Update Backend.php

--- src/Core/Setup/Backend.php
+++ src/Core/Setup/Backend.php
@@ -9,6 +9,28 @@ use Composer\Installer\PackageEvent;
 
 class Backend {
     
+	static function postUpdate(Event $event){
+		//$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+		$vendorDir = realpath($event->getComposer()->getConfig()->get('vendor-dir'));
+     	$projectDir = realpath($vendorDir . '/..');
+		$vendorDir."/composer/installed.json";
+		
+		
+		$installed = json_decode(\Botnyx\Sfe\Backend\Core\Setup\Backend::readfile($vendorDir."/composer/installed.json"));
+		
+		echo "\n";
+		echo "Updating: `".$installed[0]->name."`\n";
+		echo "version: `".$installed[0]->version."`\n";
+		//$installed[0]->version_normalized;
+		
+		
+		#var_dump("xyz");
+		echo "-----------------------------------------\n";
+		echo "Database setup/update should go here\n";
+		
+		print_r($projectDir );
+		echo "\n";
+	}
 	
 	static function postInstall(Event $event){
 		
