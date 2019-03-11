2c260d236f42ddcb80553aa3d1a291773443748a
botnyx <chlsbkrs@gmail.com>
2019-03-11 16:05:13 +0100

    Update update2.sql.php

--- src/Core/Database/updates/update2.sql.php
+++ src/Core/Database/updates/update2.sql.php
@@ -9,7 +9,7 @@ class update2 extends Botnyx\Sfe\Backend\Core\Database\updateInterface {
 	var $previousVersion = "1";
 	
 	function start(){
-		// $this->exec("sql");
+		print_r( $this->exec("SHOW TABLES;") );
 	}
 	
 	
