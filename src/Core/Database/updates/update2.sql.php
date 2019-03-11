fbd28d9635813053b18b369b8d3eaf7ee7805235
botnyx <chlsbkrs@gmail.com>
2019-03-11 16:08:25 +0100

    Update update2.sql.php

--- src/Core/Database/updates/update2.sql.php
+++ src/Core/Database/updates/update2.sql.php
@@ -2,8 +2,8 @@
 
 
 namespace Botnyx\Sfe\Backend\Core\Database\updates;
-
-class update2 extends Botnyx\Sfe\Backend\Core\Database\updateInterface {
+//extends Botnyx\Sfe\Backend\Core\Database\updateInterface
+class update2  {
 	
 	var $thisVersion = "2";
 	var $previousVersion = "1";
