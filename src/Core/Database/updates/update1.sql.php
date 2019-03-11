7bff812dd80109b53600d7edf7530566c3e4d2a6
botnyx <chlsbkrs@gmail.com>
2019-03-11 17:00:25 +0100

    ..

--- src/Core/Database/updates/update1.sql.php
+++ src/Core/Database/updates/update1.sql.php
@@ -5,8 +5,8 @@ namespace Botnyx\Sfe\Backend\Core\Database\updates;
 
 class update1 extends \Botnyx\Sfe\Backend\Core\Database\updateInterface {
 	
-	var $thisVersion = "1";
-	var $previousVersion = "1";
+	var $thisVersion = 1;
+	var $previousVersion = 1;
 	
 	
 	function start(){
--- src/Core/Database/updates/update2.sql.php
+++ src/Core/Database/updates/update2.sql.php
@@ -5,8 +5,8 @@ namespace Botnyx\Sfe\Backend\Core\Database\updates;
 //extends Botnyx\Sfe\Backend\Core\Database\updateInterface
 class update2 extends \Botnyx\Sfe\Backend\Core\Database\updateInterface {
 	
-	var $thisVersion = "2";
-	var $previousVersion = "1";
+	var $thisVersion = 2;
+	var $previousVersion = 1;
 	
 	function start(){
 		print_r( $this->exec("SHOW TABLES;") );
