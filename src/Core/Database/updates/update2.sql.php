051791f772320a507c8c624d3deb203f71fda147
botnyx <chlsbkrs@gmail.com>
2019-03-11 17:50:37 +0100

    Update update2.sql.php

--- src/Core/Database/updates/update2.sql.php
+++ src/Core/Database/updates/update2.sql.php
@@ -14,7 +14,7 @@ class update2 extends \Botnyx\Sfe\Backend\Core\Database\updateInterface implemen
 		//$this->exec($this->dbversion());
 		
 		/* finally, update the version number. */
-		//$this->exec("INSERT INTO dbversion ( version ) VALUES ('".$this->thisVersion."');");
+		$this->exec("INSERT INTO dbversion ( version ) VALUES ('".$this->thisVersion."');");
 	}
 	
 	function dbversion(){
