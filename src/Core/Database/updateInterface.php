f3f0d55c82f202d1b6c5eefdc337c05e6c10346a
botnyx <chlsbkrs@gmail.com>
2019-03-11 15:55:19 +0100

    ..

--- src/Core/Database/updateInterface.php
+++ src/Core/Database/updateInterface.php
@@ -2,7 +2,15 @@
 namespace Botnyx\Sfe\Backend\Core\Database;
 
 
-interface updateInterface { 
+interface updateInterfaceIf { 
+	
+	var $thisVersion;
+	var $previousVersion;
+	
+	function start();
+}
+
+class updateInterface  implements updateInterfaceIf { 
 	
 	var $thisVersion;
 	var $previousVersion;
@@ -11,11 +19,15 @@ interface updateInterface {
 	function __construct($pdo){
 		$this->pdo=$pdo;
 	}
-	function _checkVersion(){
-		$this->previousVersion;
-	}
-	function _update(){
-		$this->start();
+	
+	function start(){
+		
 	}
 	
+	function exec($sql){	
+		//$sql = "SELECT * FROM frontend_config WHERE client_id=:clientid";
+		$stmt = $this->pdo->prepare($sql);
+		return $stmt->execute(); // just merge two arrays
+		//$sqlResult = $stmt->fetch();
+	}
 }
--- src/Core/Database/updates/1.sql.php
+++ src/Core/Database/updates/1.sql.php
@@ -3,13 +3,14 @@
 
 namespace Botnyx\Sfe\Backend\Core\Database\updates;
 
-class update1 {
+class update2 extends Botnyx\Sfe\Backend\Core\Database\updateInterface {
 	
 	var $thisVersion = "1";
-	
 	var $previousVersion = "1";
 	
 	
-	
+	function start(){
+		// $this->exec("sql");
+	}
 	
 }
