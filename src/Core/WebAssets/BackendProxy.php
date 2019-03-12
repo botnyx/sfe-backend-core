a811fafb1575bb75a6002d4b8dc84bea83b97cbf
botnyx <chlsbkrs@gmail.com>
2019-03-13 00:10:37 +0100

    Update BackendProxy.php

--- src/Core/WebAssets/BackendProxy.php
+++ src/Core/WebAssets/BackendProxy.php
@@ -1,13 +1,39 @@
 <?php
 
+namespace Botnyx\Sfe\Backend\Core\WebAssets;
 
-namespace \Botnyx\Sfe\Backend\Core\WebAssets;
-
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class BackendProxy {
 	
 	
 	
+	function __construct(ContainerInterface $container){
+		
+	}
 	
+	function get(ServerRequestInterface $request, ResponseInterface $response, array $args = []){
+		
+		$r = explode("/",$args['path']);
+		$lastElement = end($r); 
+		if(strpos($lastElement,"sfe-")){
+			// this is a SFE combined lib.
+			
+		}else{
+			// this is a CDN request
+			//proxy $args['path']
+			
+			
+			
+			
+		}
+		
+		
+		
+		return $response->withJson( $r );
+		
+	}
 	
 }
