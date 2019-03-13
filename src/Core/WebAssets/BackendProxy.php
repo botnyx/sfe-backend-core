5dfc66a81d9a34f7324c7fe1cef18d16440c13bd
botnyx <chlsbkrs@gmail.com>
2019-03-14 00:40:38 +0100

    Update BackendProxy.php

--- src/Core/WebAssets/BackendProxy.php
+++ src/Core/WebAssets/BackendProxy.php
@@ -14,7 +14,8 @@ class BackendProxy {
 		
 		$this->proxy = new \Botnyx\Sfe\Shared\WebAssets\AssetProxy($container);
 		$this->cacher = $container->get('cache');
-		
+		$this->paths = $container->get('settings')['paths'];
+		$this->debug = true;
 	}
 	
 	function get(ServerRequestInterface $request, ResponseInterface $response, array $args = []){
@@ -22,17 +23,40 @@ class BackendProxy {
 		$r = explode("/",$args['path']);
 		$lastElement = end($r); 
 		if(strpos($lastElement,"sfe-")){
-			// this is a SFE combined lib.
+			// this is a SFE  lib.
+			
+		}elseif(strpos($lastElement,"bundle-")){
 			
 		}else{
 			// this is a CDN request
 			//proxy $args['path']
-			
+			$uri = $this->paths['cdn']."/assets/".$args['path'];
+			try{
+				return $this->proxy->get($response,$uri);	
+			}catch(\Exception $e){
+				
+				
+				//throw new \Exception("",$e->getCode());
+				//var_dump( $e->getCode() );
+				//die($uri);
+				
+				//		Botnyx\Sfe\Shared\ExceptionResponse
+				$response = \Botnyx\Sfe\Shared\ExceptionResponse::get($response,$e->getCode(),'Cdn reports: 404 Not Found');
+				return $response->withStatus($e->getCode());;
+				var_dump($x->getBody());
+				//$e->getMessage()
+				die();
+#				
+				
+				
+			}
 			
 			
 			
 		}
+		//$r['uri']=$uri;
 		
+		//print_r($this->settings);
 		
 		return  $response->withJson( $r );
 		
