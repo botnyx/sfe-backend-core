b35afd98ff9f1be98c9719a44a8ab3b6687b0a14
botnyx <chlsbkrs@gmail.com>
2019-03-14 12:44:42 +0100

    Update BackendProxy.php

--- src/Core/WebAssets/BackendProxy.php
+++ src/Core/WebAssets/BackendProxy.php
@@ -13,25 +13,62 @@ class BackendProxy {
 	function __construct(ContainerInterface $container){
 		
 		$this->proxy = new \Botnyx\Sfe\Shared\WebAssets\AssetProxy($container);
-		$this->cacher = $container->get('cache');
+		
+		// \Slim\HttpCache\CacheProvider($type = 'private', $maxAge = 86400, $mustRevalidate = false)
+		$this->cacher =new \Slim\HttpCache\CacheProvider('public', $maxAge = 86400, false);
+		
+		//$this->cacher = $container->get('cache');
 		$this->paths = $container->get('settings')['paths'];
 		$this->debug = true;
+		
+				
+		$root = $this->paths['root'];//."/vendor/botnyx/sfe-backend-js/src/sfe/";
+		$temp = $this->paths['temp'];//."/sfe-js";
+		
+		$this->sfeJS = new \Botnyx\Sfe\Javascript\sfelib($root,$temp);
+		
 	}
 	
 	function get(ServerRequestInterface $request, ResponseInterface $response, array $args = []){
 		
 		$r = explode("/",$args['path']);
 		$client_id = $r[0];
+		$lastElement = end($r); 
 		$r['_']=str_replace($r[0]."/","",$args['path']);
 		
-		$lastElement = end($r); 
+		//echo $lastElement;
+		//Thu, 08 Dec 2016 01:00:57 GMT
+		
 		if(strpos($lastElement,"sfe-")==0){
 			// this is a SFE  lib.
+			try{
+				$javascript =  $this->sfeJS->get($r['_'],array("client_id"=>$client_id));
+			}catch(\Exception $e){
+				echo $e->getMessage();
+			}
 			
 			
+			//die($args['path']);
+			
+			$response = $response->write( $javascript )->withHeader("content-type", "text/javascript");
+			$LastModified = time() + 3600;
+			
+			//$response = $response->withHeader('Cache-Control',$headerval);
+			$response = $this->cacher->withExpires($response, time() + 3600);
+			return $response = $this->cacher->withLastModified($response, $LastModified );
+			
+			
+			
+			//$response = $response->withHeader('Last-Modified',$headerval);
+			//$response = $response->withHeader('Expires',$headerval);
+			
+			//$response->withHeader('Pragma',$headerval);
+			
+			
+			die("......./.../...");
 		}elseif(strpos($lastElement,"bundle-")==0){
 			// this is a bundle request.
-			
+			$r['_TODO']="bundle-request  BackendProxy.php";
 			
 		}else{
 			// this is a CDN request
