25ec0ebf83034f14aaba292adc36135166258778
botnyx <chlsbkrs@gmail.com>
2019-03-19 16:18:14 +0100

    Update SlimLogic.php

--- src/Core/SlimLogic.php
+++ src/Core/SlimLogic.php
@@ -15,74 +15,13 @@ class SlimLogic {
 		
 		
 		
-		
 		$container['cache'] = function ($c) {
 			return new \Slim\HttpCache\CacheProvider();
 		};
 
 		
-		$container['frontendconfig'] = function($c){
-				
-			$sfeSettings = $c->get('settings')['sfe'];
-			$sfePaths = $c->get('settings')['paths'];
-			#print_r($Settings);
-
-			#die();
-			
-				#print_r($c->get('settings')['sfe']);
-			
-				/*
-					if frontend is enabled, serve it...  else 403
-				*/
-				//if(array_key_exists('sfeFrontend',_SETTINGS)){
-				// frontend remote-Config
-				// Create default HandlerStack
-				$stack = \GuzzleHttp\HandlerStack::create();
-				$stack->push(
-					  new \Kevinrob\GuzzleCache\CacheMiddleware(
-						new \Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy(
-						  new \Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage(
-							new \Doctrine\Common\Cache\FilesystemCache($sfePaths['temp'].'/guzl')
-						  )
-						)
-					  ),
-					  'cache'
-					);
-				
-				
-				#echo "<pre>";
-				#print_r( $sfePaths );
-				#print_r( $sfeSettings->clientid );
-				
-			
-			
-				$headers = ['referer' => 'https://'.$sfeSettings->hosts->frontend,'origin' => 'https://'.$sfeSettings->hosts->frontend ];
-
-				$cachedClient = new \GuzzleHttp\Client([
-					'headers' => $headers,
-					'handler' => $stack
-				]);
-
-				try{
-					$res = $cachedClient->request('GET', $sfeSettings->hosts->backend.'/api/cfg/'.$sfeSettings->clientid);
-					
-					$frontEndConfig = json_decode($res->getBody());
-				}catch(Exception $e){
-					die($e->getMessage());
-				}
-
-				echo "<pre>";
-				var_dump($frontEndConfig);
-				die($res->getBody());
-
-			die("frontend/slimlogic.php");
-				
-				return $frontEndConfig->data;
-		};
-
-		
-		
-		
+		//echo "<pre>";
+		//print_r($container);
 		
 		return $container;
 	}
